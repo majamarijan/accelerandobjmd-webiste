8b2792cff064ac56b38336c891356a5070b49848
majamarijan <majapianist@gmail.com>
2024-11-05 22:33:43 +0100

    edit docs

---
+++ docs/guidelines-for-authors/general-structure.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 1
+---
+
+# General Structure of The Page
+
+
+The manuscript or submitted paper has to meet the following criteria.
+
+
+
+
--- src/pages/submission.tsx
+++ src/pages/submission.tsx
@@ -7,7 +7,7 @@ export default function Submission() {
 				<div className='submission_title'>
 					<img
 						src={require('@site/static/img/megaphone.png').default}
-						alt='icon'
+						alt='call for papers'
 						width={128}
 					/>
 					<h2>Call for Papers</h2>
@@ -48,7 +48,9 @@ export default function Submission() {
 						</li>
 						<li>
 							Submission of proposal with title, author's affiliations, abstract
-							(up to 250 words) and keywords in English.
+							(up to 250 words), keywords in English language and the main text
+							(with or without assets, i.e. tables, images, video or/and audio
+							material).
 						</li>
 						<li>
 							Authors are usually informed within two weeks by the decision
@@ -72,39 +74,49 @@ export default function Submission() {
 					<h2>Submission Policy</h2>
 				</div>
 				<div className='submission_text'>
-					<ol>
-						<li>
-							Authors submit their manuscripts through Contact Form. Upon
-							submission, the authors will be e-mailed that their manuscripts
-							are received.
-						</li>
-						<li>
-							All abstracts of submitted manuscripts will be processed through
-							preliminary English language check. Manuscripts written in English
-							language will be examined in their entirety. The manuscripts
-							having poor grammar or English language will be given back to the
-							authors within two weeks of receipt for re-submission. If accepted
-							after re-submission, the manuscript will enter the double blind
-							peer review process, and the author will be informed about it.
-						</li>
-						<li>
-							Within a month the author will be emailed if the manuscript is
-							accepted, rejected or has to be revised. Authors who receive a
-							decision of Minor Revision or Major Revision have 20 days to
-							resubmit the revised manuscript. The revised manuscript will be
-							re-assigned to the reviewers. The refereeing procedures are to be
-							done again, and in less than two weeks the author will be emailed
-							the final decision.
-						</li>
-						Authors will be informed in a couple of days after their papers have
-						been published. Before publishing they will be able to check their
-						papers, and asked for the corrections to be made and it will be done
-						to them. Authors of manuscripts rejected at this stage will normally
-						be informed within two weeks of receipt. Manuscripts may be rejected
-						for the following reasons: if they are insufficiently original, have
-						serious scientific flaws, or are outside the aims and scope of the
-						journal.{' '}
-					</ol>
+					<h4>Initial Submission</h4>
+					<p>
+						Authors submit their manuscripts through Contact Form. Upon
+						submission, the authors will be e-mailed that their manuscripts are
+						received.
+					</p>
+					<h4>Processing of Submitted Manuscripts</h4>
+					<p>
+						All submitted manuscripts will be processed through preliminary
+						English language check. Manuscripts written in English language will
+						be examined in their entirety. The manuscripts having poor grammar
+						or English language will be given back to the authors within two
+						weeks of receipt for re-submission.
+					</p>
+					<h4>Acception and Re-Submission</h4>
+					<p>
+						Within a month the author will be emailed if the manuscript is
+						accepted, rejected or has to be revised. Authors who receive a
+						decision of Minor Revision or Major Revision have 20 days to
+						resubmit the revised manuscript. The revised manuscript will be
+						re-assigned to the reviewers. The refereeing procedures are to be
+						done again, and in less than two weeks the author will be emailed
+						the final decision.
+					</p>
+					<p>
+						If accepted (or accepted after re-submission), the manuscript will
+						enter the double blind peer review process, and the author will be
+						informed about it.
+					</p>
+					<h4>Rejection</h4>
+					<p>
+						If manuscript is outside the aims and scope of the journal it is
+						immediately rejected. Authors of manuscripts that have been rejected
+						will be informed within two weeks of their receipt. Manuscripts may
+						be rejected for the following reasons: if they are insufficiently
+						original, have serious scientific flaws like plagiarism or if the
+						manuscript is not suitable for publication for any other reason.
+					</p>
+					<h4>Publication</h4>
+					<p>
+						Before official publication, all authors will be able to check their
+						papers online, and check for possible corrections.
+					</p>
 				</div>
 			</div>
 		</Wrapper>
